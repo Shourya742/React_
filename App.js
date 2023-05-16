140f8346ad825b7f860045f8d6a5470b8e6b50f1
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2023-05-16 13:00:24 +0530

    React Fragments

--- App.js
+++ App.js
@@ -4,12 +4,17 @@ import ReactDOM from "react-dom/client";
 //Functional Component
 
 const Title = () => <h1 className="head">Hello, Breaking down react</h1>;
+const title = <h1 className="head">Hello, Breaking down react</h1>;
 
+// React Fragments;
 const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1 className="heading">React functional Component</h1>
-  </div>
+  <>
+    <div id="container1">
+      {title}
+      <h1 className="heading">React functional Component</h1>
+    </div>
+    <div id="container2"></div>
+  </>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
